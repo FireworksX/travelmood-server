f0fb17e7657c74ed733ed8e21e86813a38c39421
arturabeltins <abeltinsh.a@icloud.com>
2021-07-01 14:36:26 +0300

    pm2 dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,14 +11,14 @@ module.exports = {
       env: {
         HOST: '0.0.0.0',
         PORT: 5000,
-        NODE_ENV: 'production',
+        NODE_ENV: 'development',
       },
       // production mode
       // pm2 start ecosystem.config.js --env production
       env_production: {
         HOST: '0.0.0.0',
         PORT: 5000,
-        NODE_ENV: 'production',
+        NODE_ENV: 'development',
       },
       output: './logs/console.log',
       error: './logs/consoleError.log',
